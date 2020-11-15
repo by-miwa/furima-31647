07c5ac8f3faa9b1ba7ede58b59f548a27395d283
by-miwa <miwa.boardgame@gmail.com>
2020-11-15 20:19:11 +0900

    新規登録 正常系

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmation、first_nameとlast_name、birthdayが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上であれば登録できる" do
+        @user.password = "a00000"
+        @user.password_confirmation = "a00000"
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
