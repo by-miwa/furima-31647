f72de75ec87bd0f736aea5ae070359953f611c12
by-miwa <miwa.boardgame@gmail.com>
2020-11-16 19:56:05 +0900

    ActiveStorageを導入する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :text, :category_id, :status_id, :delivery_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
+  end
 end
