ef4b6024d49e1739db606da62c1c14d822b6982c
by-miwa <miwa.boardgame@gmail.com>
2020-11-17 21:01:12 +0900

    手順6 出品した商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:title, :text, :category_id, :status_id, :delivery_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :title, :text, :category_id, :status_id, :delivery_fee_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :area
+  belongs_to :day
+
+  validates :title, :text, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :area_id, :days_id, numericality: { other_than: 1 } 
 
   belongs_to :user
   has_one :record
   has_one_attached :image
 end
-
