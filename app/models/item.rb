a03a68b790b4ea64eaa832250715509f945f34fa
by-miwa <miwa.boardgame@gmail.com>
2020-11-18 22:03:19 +0900

    STEP2 JS金額計算

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,22 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :day
   
-  validates :image, presence: true
-  validates :title, presence: true
+  with_options presence: true do
+  validates :image
+  validates :title
+  validates :text
+  validates :price
+  validates :category_id
+  end
+
   validates :category_id, :status_id, :delivery_fee_id, :area_id, :days_id, numericality: { other_than: 1 } 
+  validates :area_id, numericality: { other_than: 0 }
+
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
   has_one :record
   has_one_attached :image
+
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,19 @@
 FactoryBot.define do
   factory :item do
-    title              { 'test' }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    title            { 'coffee' }
     text             { 'test' }
-    category_id            { '3' }
-    status_id        { '2' }
-    delivery_fee_id       { '2' }
-    area_id              { '2' }
-    days_id              { '2' }
-    price              { '300' }
-    user_id              { '2' }
+    category_id      {3}
+    status_id        {2}
+    delivery_fee_id  {2}
+    area_id          {2}
+    days_id          {2}
+    price            {300}
+
+    association :user
   end
 end
