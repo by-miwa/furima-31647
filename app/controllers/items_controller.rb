f104fde27552661eaa977ba2082660de10ef3f35
by-miwa <miwa.boardgame@gmail.com>
2020-11-19 20:07:40 +0900

    ログイン条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, except: [:index, :show]
+  
+  
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,9 +24,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to item_path
+    else
+       render :edit
+    end   
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :title, :text, :category_id, :status_id, :delivery_fee_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
   end
+
 end
