6ecc1d0803564570bbea011b0175c5daf78bee42
by-miwa <miwa.boardgame@gmail.com>
2020-11-24 14:49:46 +0900

    商品購入ページ編集

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,18 +5,18 @@ class RecordsController < ApplicationController
   end
 
   def create
-    binding.pry
-    @record_street = RecordStreet.crate(record_street_params)
+    @item = Item.find(params[:item_id])
+    @record_street = RecordStreet.new(record_street_params)
     if @record_street.valid?
       @record_street.save
-      redirect_to action: :index
+      redirect_to root_path
     else
-      render action: :new
+      render action: :index
     end
   end
 
   def record_street_params
-    params.require(:record_street).permit(:postal, :prefecture, :city, :address, :apartment, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:record_street).permit(:postal, :prefecture_id, :city, :address, :apartment, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
    end
 
 end
